f277dc667f8e8eeba047494f000797861cda9916
User <user@example.com>
2025-04-15 01:24:25 -0300

    Your commit message

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './utils/setupSchema' // Importar o script de configuração do schema
 
 // A configuração do banco agora será feita automaticamente via setupSchema.ts
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
